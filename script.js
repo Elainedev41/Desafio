a78563f4b0b5b51d725a7b6903d5ae7b9f7d0f90
Elaine <elainedevjava@gmail.com>
2022-01-15 20:57:33 -0300

    Implmentar função pegar personagem ao arquivo JavaScript

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ const imagem3 = document.querySelector('#personagem3');
 const nomeDoPersonagem3 = document.querySelector('#nome3');
 const especie3 = document.querySelector('#especie3');
 const condicao3 = document.querySelector('#status3');
+
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+}
+pegarPersonagem = (imagem, nomeDoPersonagem, especie, condicao) => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src= data.image;
+        imagem.alt= data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+    });    
+}
+
+botao.onclick = () => {pegarPersonagem(imagem1, nomeDoPersonagem1, especie1, condicao1);
+                       pegarPersonagem(imagem2, nomeDoPersonagem2, especie2, condicao2);
+                       pegarPersonagem(imagem3, nomeDoPersonagem3, especie3, condicao3)};
